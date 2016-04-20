e4a38635eac490a405c017bd60ab207b1af85347
collinsmichael <electro_hobbyist@yahoo.com>
2016-04-20 17:40:34 +0100

    TansactionPages Complete

--- GameShop/GameShop/FrontEnd/TransactionFormPage.cs
+++ GameShop/GameShop/FrontEnd/TransactionFormPage.cs
@@ -82,64 +82,9 @@ namespace GameShop {
             Dictionary<string, Widget> view = new Dictionary<string, Widget>();
             view.Add("header", new WidgetTitle("Account Transactions"));
             view.Add("transactionId",  new WidgetTextBox(272, 152, 320, 32, "", true, false, false));
-            view.Add("fees paid", new WidgetButton(464, 428, 128, 32, "fees paid", OnMakePaidClick));
+            view.Add("edit", new WidgetButton(464, 428, 128, 32, "Edit", OnViewEditClick));
             view.Add("cancel", new WidgetButton(272, 428, 128, 32, "Cancel", OnCancelClick));
             Form1.formgen.AddPage("transaction.view", view);
-        }
-
-
-        public void OnMakePaidClick(object sender, EventArgs e) {
-            // TODO : Sanitize field data (use regular expression parsing)
-            // TODO : If you're not using any of these please comment out
-            //        rather than deleting these took a lot of time to build
-            #region sanitize
-            Regex regexname = new Regex(@"^[A-Za-z]+", RegexOptions.IgnoreCase);
-            Regex regexdate = new Regex(@"^[0-9]{1,2}[-/.]{1}[0-9]{1,2}[-/.]{1}[0-9]{2,4}");
-            Regex regexvalu = new Regex(@"^[0-9]+", RegexOptions.IgnoreCase);
-            Regex regexurls = new Regex(@"^www[.][a-z]{1,15}[.](com|org)$");
-            Regex regexmail = new Regex(@"^[a-zA-Z0-9]{1,10}@[a-zA-Z]{1,10}.(com|org)$");
-
-            Match matchname = regexname.Match("louise");
-            if (matchname.Success) MessageBox.Show(matchname.Value);
-            else MessageBox.Show("No Match");
-
-            Match matchvalu = regexvalu.Match("78912");
-            if (matchvalu.Success) MessageBox.Show(matchvalu.Value);
-            else MessageBox.Show("No Match");
-
-            Match matchdate = regexdate.Match("1-1-2000");
-            if (matchdate.Success) MessageBox.Show(matchdate.Value);
-            else MessageBox.Show("No Match");
-            matchdate = regexdate.Match("1/1/2000");
-            if (matchdate.Success) MessageBox.Show(matchdate.Value);
-            else MessageBox.Show("No Match");
-            matchdate = regexdate.Match("1.1.2000");
-            if (matchdate.Success) MessageBox.Show(matchdate.Value);
-            else MessageBox.Show("No Match");
-
-            Match matchurls = regexurls.Match("www.domain.com");
-            if (matchurls.Success) MessageBox.Show(matchurls.Value);
-            else MessageBox.Show("No Match");
-
-            Match matchmail = regexmail.Match("louise@mckeown.com");
-            if (matchmail.Success) MessageBox.Show(matchmail.Value);
-            else MessageBox.Show("No Match");
-            #endregion
-
-
-            //Transaction transaction = new Transaction();
-            //int membershipfees = 0;
-            //int.TryParse((Form1.formgen.GetControl("transaction.make", "membership fee") as TextBox).Text, out membershipfees);
-            //int rentalFees = 0;
-            //int.TryParse((Form1.formgen.GetControl("transaction.make", "rental fee") as TextBox).Text, out rentalFees);
-            //int lateReturnFee = 0;
-            //int.TryParse((Form1.formgen.GetControl("transaction.make", "late return fee") as TextBox).Text, out lateReturnFee);
-            //transaction.SetMembershipFee(membershipfees);
-            //transaction.SetRentalFee(rentalFees);
-            //transaction.SetLateReturnFee(lateReturnFee);
-            //Form1.context.AddTransaction(transaction.GetTransactionId(), transaction);
-            //Form1.context.SetSelected("transaction", transaction.GetTransactionId());
-            //Form1.formgen.BuildPage("transaction.list");
         }
     }
     #endregion
@@ -165,7 +110,7 @@ namespace GameShop {
 
             edit.Add("header", new WidgetTitle("Edit Transaction"));
             edit.Add("transactionId",  new WidgetTextBox(272, 152, 320, 32, "", true, false, false));
-            edit.Add("fees paid", new WidgetButton(464, 428, 128, 32, "fees paid", OnEditSubmitClick));
+            edit.Add("submit", new WidgetButton(464, 428, 128, 32, "Submit", OnEditSubmitClick));
             edit.Add("cancel", new WidgetButton(272, 428, 128, 32, "Cancel", OnCancelClick));
             Form1.formgen.AddPage("transaction.edit", edit);
         }
